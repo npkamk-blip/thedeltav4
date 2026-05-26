c3fb97d4710ef91d0b7aede6b2a6dfbce87bc0b7
npkamk-blip <npkamk@gmail.com>
2026-05-26 08:55:41 -0400

    Update main.py

--- scanner/main.py
+++ scanner/main.py
@@ -163,7 +163,8 @@ def get_last_trading_day():
         date(2025,4,18),date(2025,5,26),date(2025,6,19),date(2025,7,4),
         date(2025,9,1),date(2025,11,27),date(2025,12,25),
         date(2026,1,1),date(2026,1,19),date(2026,2,16),date(2026,4,3),
-        date(2026,5,26),
+        date(2026,5,25),date(2026,6,19),date(2026,7,3),date(2026,9,7),
+        date(2026,11,26),date(2026,12,25),
     }
     for _ in range(7):
         if check.weekday() < 5 and check not in holidays:
